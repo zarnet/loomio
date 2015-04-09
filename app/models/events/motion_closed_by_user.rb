fdcd59a8c0421b80b3c565061d38cbfca263023c
Robert Guthrie <rob@guthr.ie>
2015-04-09 17:52:20 +1200

    e2e specs work again

--- app/models/events/motion_closed_by_user.rb
+++ app/models/events/motion_closed_by_user.rb
@@ -1,6 +1,4 @@
 class Events::MotionClosedByUser < Events::MotionClosed
-  after_create :notify_users!
-
   def self.publish!(motion, closer)
     create!(kind: "motion_closed_by_user",
             eventable: motion,
